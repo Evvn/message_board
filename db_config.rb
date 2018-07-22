e38946077c239efa96bcc55356fd295d211c8a73
Evan <evvn@outlook.com>
2018-07-23 08:57:04 +1000

    added database url to db config

--- db_config.rb
+++ db_config.rb
@@ -10,4 +10,4 @@ options = {
 
 
 # add this line before going live
-ActiveRecord::Base.establish_connection( ENV['DATABASE_URL'] || options)
+ActiveRecord::Base.establish_connection( ENV['postgres://rqxuxbjdyfctkx:2c5ef8776fbe3f236206b5584562b0c45c989928053fe0f1e92c92d6e6a8cd5e@ec2-54-235-66-24.compute-1.amazonaws.com:5432/ddua5qh1quh32b'] || options)
